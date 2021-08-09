32dede8f78e7aaafb262d3a8a3dc1b4a075e87a1
Nhan-Vo <vominhnhannick@gmail.com>
2021-08-09 10:44:21 +0700

    Added destinations and travelers

---
+++ Boredless.py
@@ -0,0 +1,4 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt", "Tây Ninh, Việt Nam"]
+traveler1 = ['Trần Cao Kỳ Duyên', 'Tây Ninh, Việt Nam',['food', 'beautiful view']]
+traveler2 = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
