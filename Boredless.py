bfcec155d475c20779f6faeb37c5c2b43bd37f89
Nhan-Vo <vominhnhannick@gmail.com>
2021-08-09 11:41:01 +0700

    Added logic to convert destination to internal data

--- Boredless.py
+++ Boredless.py
@@ -1,4 +1,8 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt", "Tây Ninh, Việt Nam"]
 traveler1 = ['Trần Cao Kỳ Duyên', 'Tây Ninh, Việt Nam',['food', 'beautiful view']]
 traveler2 = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+def get_des_index(destination):
+    index = destinations.index(destination)
+    return index
+print(get_des_index(traveler1[1]))
 
